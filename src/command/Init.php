8042b3b1211a46019ba003e623d4664c9bc629a8
zsl <jszsl001@163.com>
2024-04-06 17:32:40 +0800

    自动创建目录

--- src/command/Init.php
+++ src/command/Init.php
@@ -18,10 +18,22 @@ class Init extends Command
     {
         # 复制code下的文件到各自目录
         $code_path = dirname(__DIR__) . '/code/';
+
+        // 如果目录不存在则新建
+        if (!is_dir(app() -> getAppPath() . 'common/logic')) {
+            mkdir(app() -> getAppPath() . 'common/logic', 0777, true);
+        }
+        if (!is_dir(app() -> getAppPath() . 'common/model/traits')) {
+            mkdir(app() -> getAppPath() . 'common/model/traits', 0777, true);
+        }
+        if (!is_dir(app() -> getAppPath() . 'common/controller')) {
+            mkdir(app() -> getAppPath() . 'common/controller', 0777, true);
+        }
+
         copy($code_path . 'BaseLogic.php', app() -> getAppPath() . 'common/logic/BaseLogic.php');
         copy($code_path . 'CommFunc.php', app() -> getAppPath() . 'common/model/traits/CommFunc.php');
-        copy($code_path . 'StatusCode.php', app() -> getAppPath() . 'StatusCode.php');
         copy($code_path . 'RestBaseController.php', app() -> getAppPath() . 'common/controller/RestBaseController.php');
+        copy($code_path . 'StatusCode.php', app() -> getAppPath() . 'StatusCode.php');
 
     }
 
